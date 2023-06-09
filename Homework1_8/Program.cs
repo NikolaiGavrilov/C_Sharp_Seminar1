d46c7ce45ba8fa39fc0b1472a9a21d8362e2f089
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-09 20:58:01 +0300

    Completed Exercise 8

---
+++ Homework1_8/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter any number: ");
+int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+int initialNumber = 1;
+int firstEvenNumber = 2;
+
+if (yourNumber > initialNumber)
+{
+    if (yourNumber % 2 == 0)
+    {
+        while (firstEvenNumber < yourNumber)
+        {
+          Console.Write(firstEvenNumber + ", ");
+          firstEvenNumber = firstEvenNumber + 2;
+        }
+        Console.Write(yourNumber);
+    }
+    else
+        while (firstEvenNumber < yourNumber)
+        {
+          Console.Write(firstEvenNumber + ", ");
+          firstEvenNumber = firstEvenNumber + 2;
+        }
+}
+else
+{
+    if (yourNumber != 0)
+    {
+        Console.WriteLine("You have a NEGATIVE number. It DOESN'T BELONG to natural numbers! ");
+        Console.Write("Try again with another number and MAKE SURE THAT YOUR NEW NUMBER IS NATURAL.");   
+    }
+    else
+    {
+        Console.Write("0");
+    }
+}
