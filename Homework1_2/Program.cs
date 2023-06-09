fa2bc522a70d5dcee659761199bb003b3b458a11
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-09 17:03:53 +0300

    Completed Exercise 2

---
+++ Homework1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2. Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.WriteLine("Enter first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA != numberB)
+{
+    if (numberA > numberB)
+    {
+        Console.WriteLine(numberA + " is BIGGER than " + numberB);
+        Console.WriteLine("First number is MAX and second number is MIN");
+    }
+    else
+    {
+       Console.WriteLine(numberB + " is BIGGER than " + numberA);
+       Console.WriteLine("Second number is MAX and first number is MIN");
+    }
+}
+else
+{
+    Console.WriteLine(numberA + " is equal to " + numberB);
+    Console.WriteLine("First number and second number are EQUAL numbers!");
+}
